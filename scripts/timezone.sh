9db87b20d7d5ef53e661b9f2721ff3487ae9a89b
ntoofu <ntoofu@users.noreply.github.com>
2016-12-05 21:45:42 +0900

    [add] support timezone designation

--- scripts/timezone.sh
+++ scripts/timezone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 chroot /mnt/gentoo /bin/bash -e <<'EOF'
-ln -snf /usr/share/zoneinfo/UTC /etc/localtime
-echo UTC > /etc/timezone
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+echo $TZ > /etc/timezone
 EOF
